f3fd9a102325204ec69f7905c445e4c27c0256d4
mirunaNG <miruna.ng@gmail.com>
2025-03-05 10:57:48 +0100

    Revert "rutas"
    This reverts commit 6dbf967f6e696df01f7886f2775d89f64b02874b.

--- Login/logout.php
+++ Login/logout.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../CSS/estilo.css" rel="stylesheet" type="text/css">
+        <link href="\AW\EasyCheckIn\CSS\estilo.css" rel="stylesheet" type="text/css">
         <link href="../CSS/headerFooter.css" rel="stylesheet" type="text/css">
         <title>Login</title>
     </head>
