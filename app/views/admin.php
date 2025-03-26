88f9a6c4d195fa3d1ab4ac4e09e07b4f9a80ad46
mirunaNG <miruna.ng@gmail.com>
2025-03-26 11:15:49 +0100

    botones

--- app/views/admin.php
+++ app/views/admin.php
@@ -1,10 +1,12 @@
 <div class="container">
     <h1>Panel de Administración</h1>
     <p>Bienvenido al panel de administración. Aquí puedes gestionar el contenido del sitio.</p>
-    <ul>
-        <li><a href="?action=mostrar_usuarios">Gestión de Usuarios</a></li>
-        <li><a href="?action=mostrar_reservas">Gestión de Reservas</a></li>
-    </ul>
+    <div class="d-flex flex-column gap-2">
+        <form action="" method="get">
+            <a href="?action=mostrar_reservas" class="btn">Gestion Reservas</a>
+            <a href="?action=mostrar_usuarios" class="btn">Gestion Usuarios</a>
+        </form>
+    </div>
 </div>
 
 <!-- CREDENCIALES DEL ADMIN
